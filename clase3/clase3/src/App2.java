34d1da5e4aa0a0f23b1f818d47e2973ade42ac21
Mancaceresuade <mancaceres@uade.edu.ar>
2025-08-20 19:41:48 -0300

    ejercicio complejidad algoritmo recursivo

--- clase3/clase3/src/App2.java
+++ clase3/clase3/src/App2.java
@@ -7,4 +7,24 @@ public class App2 {
         todasLasColumnasConUnMultiploDe(matriz,4); // false
     }
 
+    private static boolean todasLasColumnasConUnMultiploDe(int[][] matriz, int numero) {
+        return todasLasColumnasConMultipoDeRec(matriz, numero, 0);
+    }
+
+    // Revisa cada columna de la matriz
+    private static boolean todasLasColumnasConMultipoDeRec(int[][] matriz, int numero, int columna) {
+        if (columna == matriz[0].length) return true; // revisamos todas las columnas
+        if (!columnaConMultipoDeRec(matriz, numero, matriz.length - 1, columna)) {
+            return false; // si alguna columna no tiene múltiplo → false
+        }
+        return todasLasColumnasConMultipoDeRec(matriz, numero, columna + 1);
+    }
+
+    // Revisa si una columna tiene al menos un múltiplo
+    private static boolean columnaConMultipoDeRec(int[][] matriz, int numero, int fila, int columna) {
+        if (fila < 0) return false; // no encontró múltiplo en esa columna
+        if (matriz[fila][columna] % numero == 0) return true; // encontró múltiplo
+        return columnaConMultipoDeRec(matriz, numero, fila - 1, columna);
+    }    
+
 }
